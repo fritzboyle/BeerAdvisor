594739936f0b80a856f233da76db507df25016d9
fritzboyle <fritzboyle@ZenBook2023.local>
2024-11-21 17:32:54 -0800

    Initial commit

---
+++ BeerAdvisor/BeerAdvisorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeerAdvisorApp.swift
+//  BeerAdvisor
+//
+//  Created by Fritz Boyle on 11/21/24.
+//
+
+import SwiftUI
+
+@main
+struct BeerAdvisorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeerAdvisorTests/BeerAdvisorTests.swift
@@ -0,0 +1,17 @@
+//
+//  BeerAdvisorTests.swift
+//  BeerAdvisorTests
+//
+//  Created by Fritz Boyle on 11/21/24.
+//
+
+import Testing
+@testable import BeerAdvisor
+
+struct BeerAdvisorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
